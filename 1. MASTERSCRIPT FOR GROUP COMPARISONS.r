3ebe4868946aacd65713526617e1afbabcdc79a9
Juan A. Arias <34753691+iguanamarina@users.noreply.github.com>
2024-06-19 13:53:17 +0100

    Once again

---
+++ 1. MASTERSCRIPT FOR GROUP COMPARISONS.r
@@ -0,0 +1,16 @@
+############################## ################### ############## ### 
+##
+## Script name: 1. MASTERSCRIPT FOR GROUP COMPARISONS.r
+##
+## Purpose of script: Use SCCs for the estimation of regions with different
+##                    activity levels in PET images and also to compare the
+##                    results with SPM.
+##
+## Date Created: 2024-06-19
+##
+## Author: Juan A. Arias (M.Sc.)
+## Email: juanantonio.arias.lopez@usc.es
+## Webpage: https://juan-arias.xyz
+##   
+############################## ################### ############## ### 
+
